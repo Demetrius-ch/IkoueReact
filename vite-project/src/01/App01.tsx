dba60cf09d3813cf7cb2be1441ecb64e8a99de67
Demetrius-ch <lgdemetrius@gmail.com>
2025-01-31 17:36:20 +0100

    first commit

---
+++ vite-project/src/01/App01.tsx
@@ -0,0 +1,14 @@
+import img from './Cyberattaqua.jpg'
+export default function App01() {
+    return (
+        
+        <div className="t01">
+            <div>
+                <img src={img} alt="" width={500} />
+            </div>
+           
+        Mon premier composant React
+        </div>
+
+    )
+}
---
+++ vite-project/src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import App01 from './01/App01.tsx'
+import './01/style.css'
+
+
+createRoot(document.getElementById('root')!).render(
+  <App01 /> // Replace 'App01' with the actual component name
+ 
+)
